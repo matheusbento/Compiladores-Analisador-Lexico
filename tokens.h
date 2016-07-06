841c0adf68c54d771bfa632e68eaf23ba28ec018
Matheus Bento <knightadm@hotmail.com>
2016-07-06 08:01:34 -0300

    Add files via upload

---
+++ tokens.h
@@ -0,0 +1,37 @@
+#define VAR 1;
+#define ENDL 2;
+
+// saida
+#define OUT 3;
+
+// in
+#define IN 4;
+
+// var
+#define ID 5;
+
+// numeros
+#define NUMBER 6;
+#define NUMBERFL 7;
+
+// operacoes
+#define SOMA 8;
+#define MENOS 9;
+#define DIV 10;
+#define MULT 11;
+
+// cochetes
+#define OC 12;
+#define CC 13;
+
+// parenteses
+#define OP 14;
+#define CP 15;
+
+// comparações
+#define LT		101
+#define LE		102
+#define EQ		103
+#define NE		104
+#define GT		105
+#define GE		106
