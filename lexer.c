16c31f9241dabd7a5bcee56fca593c7f0d9a9dc9
Matheus Bento <knightadm@hotmail.com>
2016-07-06 08:02:10 -0300

    Add files via upload

---
+++ lexer.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokens.h"
+
+extern int yylex();
+extern int relop_att;
+extern int nline;
+extern char* yytext;
+
+int main()
+{
+	int ntoken;
+   char* d = (char*) malloc(2);
+	ntoken = yylex();
+	while(ntoken) {
+		switch(ntoken){
+			case RELOP:
+            switch (relop_att) {
+               case LT: d = "LT"; break;
+               case LE: d = "LE"; break;
+               case EQ: d = "EQ"; break;
+               case NE: d = "NE"; break;
+               case GT: d = "GT"; break;
+               case GE: d = "GE"; break;
+            }
+				printf("<RELOP,%s>", d );
+				break;
+			case IF:
+				printf("<IF>");
+				break;
+			case THEN:
+				printf("<THEN>");
+				break;
+			case ELSE:
+				printf("<ELSE>");
+				break;
+			case NUMBER:
+				printf("<NUMBER,%s>", yytext);
+				break;
+			case ID:
+				printf("<ID,%s>", yytext);
+				break;
+         case OP:
+   				printf("<OP>");
+   				break;
+         case CL:
+               printf("<CL>");
+               break;
+		}
+      printf("\n");
+		ntoken = yylex();
+	}
+	printf("\n EOF - Lines: %d\n", nline);
+	return 0;
+}
